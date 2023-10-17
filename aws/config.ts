2c26b244364fc6e634ebb490162273e5fb324f9d
Abhishek <coderabhishek5@gmail.com>
2023-10-17 13:25:58 +0530

    few more changes

--- aws/config.ts
+++ aws/config.ts
@@ -1,10 +1,10 @@
 import { S3Client } from "@aws-sdk/client-s3";
 
 const s3 = new S3Client({
-  region: process.env.AWS_REGION as string,
+  region: process.env.REGION as string,
   credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY as string,
-    secretAccessKey: process.env.AWS_SECRET_KEY as string,
+    accessKeyId: process.env.ACCESS_KEY as string,
+    secretAccessKey: process.env.SECRET_KEY as string,
   },
 });
 
